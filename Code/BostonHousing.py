c3bf81f555d001324aa7d960fdd3f0141afb4bec
RanfordS <wemustbemental@gmail.com>
2020-01-28 11:08:05 +0000

    Fix and completed single layer Boston Housing

--- Code/BostonHousing.py
+++ Code/BostonHousing.py
@@ -1,24 +1,33 @@
+## Imports
 import numpy as np
+import matplotlib.pyplot as plt
 # import keras so that we can access the Boston housing data
 from tensorflow import keras
 
-# parameters
-num_epochs = 10000
-eta = 0.001
-inputs = (5,8,12,1) # end index is a dummy for the bais
+##### Settings #####
 
-# load the data and crop it
+## Parameters
+num_epochs = 10
+eta = 0.001
+# which columns to use
+# last column will be replaced with the bias
+inputs = (5,8,12,1) 
+
+## Plot settings
+plt.rc ('text', usetex=True)
+plt.rc ('font', family='serif')
+
+##### End of settings #####
+
+## Load the data and crop it
 num_inputs = len (inputs)
 dataset = keras.datasets.boston_housing
 (train_X, train_y), (test_X, test_y) = dataset.load_data (test_split = 0)
 train_X = train_X[:,inputs]
 train_X[:,-1] = 1
-#test_X  = test_X [:,inputs]
 num_train = train_X.shape[0]
 
-print (train_X)
-
-# normalize data
+## Normalize data
 for i in range (num_inputs - 1):
     train_X[:,i] = (train_X[:,i] - train_X[:,i].mean()) / train_X[:,i].std()
 miny = min (train_y)
@@ -27,45 +36,45 @@ mean = (maxy + miny)/2
 std =  (maxy - miny)/2
 train_y = (train_y - mean)/std
 
-print (train_X)
-
-# initialise weights with random values
+## Initialise weights with random values
 w = np.array ([0.1*np.random.uniform () for i in inputs])
 
-# single layer
+## Initial error value
 y1 = np.tanh (np.array([np.dot (train_X[i,:], w) for i in range (num_train)]))
 e1 = train_y - y1
+mse_start = e1.var ()
 
-# trackers
+## Main loop
 W = [w]
-mse = e1.var()
-print ("mean square err: {0}".format (mse))
-
 k = 0
-for m in range (num_epochs):
-    k += 1
-    #epoch_err = 0
-    for n in range (num_inputs):
+for _ in range (num_epochs):
+    for n in range (num_train):
         yk = np.tanh (np.dot (train_X[n,:], w))
         err = yk - train_y[n]
-        #epoch_err += abs (err)
         g = train_X[n,:] * ((1 - yk**2)*err)
         w = w - eta*g
         W.append (w)
-        #print (err)
-        #print (w)
-    #print ("epocherr: {0}".format (epoch_err))
+        k += 1
 
-# single layer
+## Final error value
 y2 = np.tanh (np.array([np.dot (train_X[i,:], w) for i in range (num_train)]))
 e2 = train_y - y2
+mse_end = e2.var()
 
-print (0)
+## Display results
+# weights
+print ("Initial weights")
 print (W[0])
-print (k)
+print ("Final weights:")
 print (W[-1])
-# trackers
-mse = e2.var()
-print ("mean square err: {0}".format (mse))
+# error values
+print ("Initial mean square err: {0}".format (mse_start))
+print ("Final mean square err:   {0}".format (mse_end))
+# plot of weights against iterations
+W = np.array (W)
+K = range (k+1)
+for i in range (num_inputs):
+    plt.plot (K, W[:,i].tolist())
+plt.show ()
 
-
+##### EOF #####

7c8bb09a73f4d8aad85e6a9f1d68181a8638155f
RanfordS <wemustbemental@gmail.com>
2020-02-01 18:58:26 +0000

    Report work, main ToR

--- Doc/MakeDoc.lua
+++ Doc/MakeDoc.lua
@@ -1,7 +1,8 @@
+make_pdf = "pdflatex --shell-escape --interaction=nonstopmode Main.tex"
 
-os.execute ("pdflatex --interaction=nonstopmode Main.tex")
+os.execute (make_pdf)
 os.execute ("biber Main")
-os.execute ("pdflatex --interaction=nonstopmode Main.tex")
+os.execute (make_pdf)
 
 Clean = {"aux", "nlo", "out", "bbl", "bcf", "blg", "xml"}
 for _, extension in ipairs (Clean) do

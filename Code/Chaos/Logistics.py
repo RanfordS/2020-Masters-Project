0c7c2f5fcb5acb07c7c19d9bcbd4b0663ec02202
RanfordS <wemustbemental@gmail.com>
2020-04-28 21:17:42 +0100

    Unsuccessful attempt to create corrected chaos LSTM

--- Code/Chaos/Logistics.py
+++ Code/Chaos/Logistics.py
@@ -9,92 +9,63 @@ import tensorflow as tf
 
 x_0 = 0.1        # starting value
 num = 1000       # total number of samples
-frame = 10       # number of samples in a timeframe
-split_ft = 0.8   # fitting/testing split
 neurons = 64     # number of LSTM neurons
-epochs = 100     # number of epochs
-batch = 16       # training batch size
-split_vt = 0.1   # validation/training split
+epochs = 10     # number of epochs
+
+frame = 100      # number of samples in a sequence
+batch = 20       # training batch size
+
 tf.random.set_seed (123456)
+
+save_dir = "./SaveLogistics"
 
 ## End of settings
 
-## Generate chaotic data
+def build_model (batch_size):
+    l = tf.keras.layers
+    return tf.keras.Sequential (
+    [   l.LSTM (neurons, return_sequences = True, stateful = True)
+    ,   l.Dense (1)
+    ])
+
+# Generate chaotic data
+
+print ("Generating data")
 x_all = [x_0]
 for i in range (num-1):
     x_0 = 4*x_0*(1 - x_0)
     x_all.append (x_0)
 x_all = np.array (x_all)
 
-#plt.plot (x)
-#plt.show ()
+print ("Forming dataset")
+dataset_x = tf.data.Dataset.from_tensor_slices (x_all)
+sequences = dataset_x.batch (frame, drop_remainder = True)
+print (sequences)
+def split (chunk):
+    return chunk[:-1], chunk[1:]
+dataset = sequences.map (split)
+print (dataset)
+dataset = dataset.shuffle (10000).batch (batch, drop_remainder = True)
+print (dataset)
 
+# Training model
 
-## Prepare data
-# need (S,T,C,) array, where
-#  S = number of samples
-#  T = timesteps per sample
-#  C = channels (1)
-# for x
+def loss_scc_log (labels, logits):
+    scc = tf.keras.losses.sparse_categorical_crossentropy
+    return scc (labels, logits, from_logits = True)
 
-# create separate x and y sets with batched frames
-x = []
-y = []
-for i in range (num - frame):
-    f = []
-    for j in range (frame):
-        f.append ([x_all[i + j]])
-    x.append (f)
-    y.append (x_all[i + frame])
-x = np.array (x)
-y = np.array (y)
-num -= frame
-# split x and y into fitting and testing
-fitt_num = int (num*split_ft)
-fitt_x = x[0:fitt_num]
-fitt_y = y[0:fitt_num]
-test_num = num - fitt_num
-test_x = x[fitt_num:num]
-test_y = y[fitt_num:num]
+#tlstm = tf.keras.layers.LSTM(16,return_sequences=True,stateful=True)
+#tin = np.random.uniform(size=(1,2,1))
+#print (tlstm (tin))
 
-fitt_x += np.random.uniform (-0.10, 0.10, fitt_x.shape)
+print ("Building model")
+model = build_model (batch)
+model.compile (optimizer = 'adam', loss = loss_scc_log)
+model.build (tf.TensorShape ([batch, 1]))
+model.summary ()
 
-## Build model
-
-layers = tf.keras.layers
-model = tf.keras.Sequential ()
-
-model.add (layers.LSTM (neurons, stateful = False))
-#model.add (layers.Flatten (input_shape = fitt_x[0].shape))
-#model.add (layers.Dense (neurons, activation = 'tanh'))
-#model.add (layers.Dense (neurons, activation = 'tanh'))
-
-model.add (layers.Dense (1))
-model.compile (loss = 'mean_squared_error', optimizer = 'Adam')
-
-## Train
-
-results = model.fit (fitt_x, fitt_y,
-                     epochs = epochs,
-                     batch_size = batch,
-                     validation_split = split_vt,
-                     shuffle = False)
-
-## Results
-
-# plot error history
-plt.plot (results.history['loss'], label = "Loss")
-plt.plot (results.history['val_loss'], label = "Validation")
-plt.legend ()
-plt.show ()
-
-# plot prediction graph
-pred_y = model.predict (test_x)[:,0]
-print (pred_y.shape)
-print (test_y.shape)
-pred_err = np.sqrt (sum ((pred_y - test_y)**2))
-print ("Prediction error", pred_err)
-plt.plot (pred_y, label = "Prediction")
-plt.plot (test_y, label = "Actual")
-plt.legend ()
-plt.show ()
+print ("Training")
+save_pre = os.path.join (save_dir, "ckpt_{epoch}")
+save_call = tf.keras.callbacks.ModelCheckpoint
+save_call = save_call (filepath = save_pre, save_weights_only = True)
+history = model.fit (dataset, epochs = epochs, callbacks = [save_call])

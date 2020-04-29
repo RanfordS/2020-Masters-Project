05c09297f5f19e303a772729e6a7cad62b54b79f
RanfordS <wemustbemental@gmail.com>
2020-04-29 18:59:18 +0100

    Reinforcement learning attempt

---
+++ Code/RLTest/Phonetics.py
@@ -0,0 +1,14 @@
+words =
+{   "thank": "θˈæŋk"
+,   "hello": "həlˈoʊ"
+,   "the": "ðə"
+,   "quick": "kwˈɪk"
+,   "brown": "bɹˈaʊn"
+,   "fox": "fˈɑks"
+,   "jumped": "dʒˈʌmpt"
+,   "over": "ˈoʊvɛɹ"
+,   "lazy": "lˈeɪzi"
+,   "dog": "dˈɔg"
+,   "salt": "sˈɔlt"
+,   "sugar": "ʃˈʊgɛɹ"
+}
